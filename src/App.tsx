d865d12f3571351fd7aaed1f49b9920ef293bb01
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-05 00:46:36 +1100

    Add tailwindcss to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import profileThumb from "./assets/images/danny.png";
+import ring from "./assets/images/ring.png";
 
 function App() {
   return (
     <div className="page">
       <div className="content">
-        <div className="ring"></div>
+        <img src={ring} alt="ring" className="ring" />
         <img src={profileThumb} alt="Danny Bao" className="profile" />
         <div className="name">Danny Bao</div>
       </div>
