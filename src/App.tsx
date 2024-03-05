53a3395a30f6e062d89fc8957a1e836b70d81031
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-05 18:18:24 +1100

    Add react-icons package, tailwind styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,26 @@ import ring from "./assets/images/ring.png";
 function App() {
   return (
     <div className="page">
-      <div className="content">
-        <img src={ring} alt="ring" className="ring" />
-        <img src={profileThumb} alt="Danny Bao" className="profile" />
-        <div className="name">Danny Bao</div>
+      <div className="">
+        <header className="flex flex-col items-center justify-center">
+          <img
+            src={ring}
+            alt="ring"
+            className="absolute max-w-fit top-0.5 -z-10"
+            width={"100%"}
+          />
+          <img
+            src={profileThumb}
+            alt="Danny Bao"
+            className="mt-40 md:mt-60 relative"
+            height={200}
+            width={200}
+          />
+          <h1 className="name text-4xl md:text-5xl my-6">Danny Bao</h1>
+          <h3 className="usernames text-gray-600 -z-20">
+            DannyBoy5930 / ponktoku / Listless / ?
+          </h3>
+        </header>
       </div>
     </div>
   );
