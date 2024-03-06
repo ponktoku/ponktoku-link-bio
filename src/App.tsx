594e4568eddc6934f5aaffe560bb3e325ef496c2
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-06 15:28:41 +1100

    Add linkedin link and add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import profileThumb from "./assets/images/danny.png";
 import ring from "./assets/images/ring.png";
-import { FaHome } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaHome, FaGithub, FaLinkedin } from "react-icons/fa";
 import Button from "./components/Button";
 
 function App() {
@@ -26,15 +25,17 @@ function App() {
             height={200}
             width={200}
           />
-          <h1 className="name text-4xl md:text-5xl my-6">Danny Bao</h1>
-          <h3 className="usernames text-gray-600 -z-20">
+          <h1 id="name" className="text-4xl md:text-5xl my-6">
+            Danny Bao
+          </h1>
+          <h3 id="usernames" className="text-gray-600 -z-20">
             DannyBoy5930 / ponktoku / Listless / ?
           </h3>
         </header>
 
         <main className="mt-20 font-bold flex flex-col gap-5 justify-center items-center">
           {/* <div>
-            <a className="bg-[#2B2B2B] opacity-90 px-40 py-3 rounded-full border-white border-2 hover:text-[#2B2B2B] hover:border-[#E1E05B] hover:bg-[#E1E05B]">
+            <a className="bg-[#2B2B2B] opacity-90 px-40 py-3 rounded-full border-white border-2 hover:text-[#2B2B2B] hover:border-[#e1e05b] hover:bg-[#E1E05B]">
               <FaHome className="inline-block mr-2"></FaHome>
               <button>Official Website</button>
             </a>
@@ -49,7 +50,37 @@ function App() {
             href="https://github.com/DannyBoy5930"
             icon={<FaGithub />}
           />
+          <Button
+            buttonText="Linkedin"
+            href="https://www.linkedin.com/in/danny-bao/"
+            icon={<FaLinkedin />}
+          ></Button>
         </main>
+        <div className="mt-40">
+          <div className="text-9xl">--</div>
+        </div>
+        <footer className="absolute bottom-1 text-gray-600">
+          <p>
+            &#169; 2024 Danny Bao. Made with{" "}
+            <a
+              href="https://react.dev/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#e1e05b]"
+            >
+              React
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://tailwindcss.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#e1e05b]"
+            >
+              Tailwind
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
