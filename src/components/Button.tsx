bac57eca0804a91edadfcf0df075216ce1c00fdd
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-06 19:28:44 +1100

    Remove old icon and add comment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,6 @@ export default function Button({ buttonText, href, icon }: ButtonProps) {
         className="bg-[#2B2B2B] text-center hover:shadow-lg hover:shadow-[#E1E05B]/40 w-[300px] md:w-[500px] opacity-90 p-3 rounded-full 
         border-white border-2 hover:text-[#2B2B2B] hover:border-[#E1E05B] hover:bg-[#E1E05B]"
       >
-        {/* <Icon className="inline-block mr-2"></Icon> */}
         {icon && <span className="inline-block mr-2 align-middle">{icon}</span>}
         <button>{buttonText}</button>
       </a>
